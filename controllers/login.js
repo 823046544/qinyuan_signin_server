c1abf76fefe786512e5900950401555155a531c4
lzh <823046544@qq.com>
2019-08-04 11:14:03 +0800

    init

--- controllers/login.js
+++ controllers/login.js
@@ -5,8 +5,12 @@ module.exports = async (ctx, next) => {
     // 通过 Koa 中间件进行登录之后
     // 登录信息会被存储到 ctx.state.$wxInfo
     // 具体查看：
+    if (ctx.state.$wxInfo.loginState) {
+        ctx.state.data = ctx.state.$wxInfo.userinfo
+        ctx.state.data['time'] = Math.floor(Date.now() / 1000)
+    }
 
-    const { open_id } = ctx.query
+    open_id = ctx.state.$wxInfo.userinfo.userinfo.openId
     var temp = await mysql('Users').where('open_id', open_id)
 
     if (temp.length == 0) {
